c9bfe17ab92749f66007ae0900e7cb3ed8d08787
Joshua Buhler <josh@buhler.me>
2020-03-17 12:06:27 -0600

    Animation via stepper

--- Project_6/Project_6/ContentView.swift
+++ Project_6/Project_6/ContentView.swift
@@ -13,26 +13,43 @@ struct ContentView: View {
     @State private var animationAmount:CGFloat = 1.0
     
     var body: some View {
-        Button("Tap Me") {
-            //            self.animationAmount += 1.0
-        }
-        .padding(40)
-        .background(Color.red)
-        .foregroundColor(Color.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(Color.green)
-                .scaleEffect(animationAmount)
-                .opacity(Double(2 - animationAmount))
-                .animation(
-                    Animation.easeOut(duration: 5)
-                        .repeatForever(autoreverses: false)
-            )
-        )
-            .onAppear {
-                print("on appear")
-                self.animationAmount = 2
+//        Button("Tap Me") {
+//            //            self.animationAmount += 1.0
+//        }
+//        .padding(40)
+//        .background(Color.red)
+//        .foregroundColor(Color.white)
+//        .clipShape(Circle())
+//        .overlay(
+//            Circle()
+//                .stroke(Color.green)
+//                .scaleEffect(animationAmount)
+//                .opacity(Double(2 - animationAmount))
+//                .animation(
+//                    Animation.easeOut(duration: 5)
+//                        .repeatForever(autoreverses: false)
+//            )
+//        )
+//            .onAppear {
+//                print("on appear")
+//                self.animationAmount = 2
+//        }
+        
+        
+        VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                self.animationAmount += 1.0
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(Color.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            
         }
         
         
