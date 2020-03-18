5e10bc2ec80a02f51c18f5063970bb1d5421fc0b
Joshua Buhler <josh@buhler.me>
2020-03-18 14:59:21 -0600

    Controlling the animation stack

--- Project_6/Project_6/ContentView.swift
+++ Project_6/Project_6/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     
 //    @State private var animationAmount:CGFloat = 1.0
     @State private var animationAmount:Double = 1.0
+    @State private var enabled = false
     
     var body: some View {
 //        Button("Tap Me") {
@@ -57,16 +58,27 @@ struct ContentView: View {
 //
 //        }
         
+//        Button("Tap Me") {
+//            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+//                self.animationAmount += 360
+//            }
+//        }
+//        .padding(50)
+//        .background(Color.red)
+//        .foregroundColor(Color.white)
+//        .clipShape(Circle())
+//        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z:1))
+        
         Button("Tap Me") {
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                self.animationAmount += 360
-            }
+            // do nothing
+            self.enabled.toggle()
         }
-        .padding(50)
-        .background(Color.red)
-        .foregroundColor(Color.white)
-        .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z:1))
+        .frame(width: 200, height: 200)
+        .background(enabled ? Color.green : Color.red)
+        .animation(nil)
+        .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
         
     }
 }
