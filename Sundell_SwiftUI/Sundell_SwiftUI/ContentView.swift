a9729af9e19db8fa5854c8c8856196608b12de9c
Joshua Buhler <josh@buhler.me>
2020-03-24 09:24:05 -0600

    Checking out Sundell's article as well
    https://www.swiftbysundell.com/articles/swiftui-layout-system-guide-part-1/

---
+++ Sundell_SwiftUI/Sundell_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sundell_SwiftUI
+//
+//  Created by Joshua Buhler on 3/24/20.
+//  Copyright © 2020 jb. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
