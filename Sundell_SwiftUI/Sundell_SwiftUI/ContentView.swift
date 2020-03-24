616882567861a7081a0fca0cb0464137b1cec776
Joshua Buhler <josh@buhler.me>
2020-03-24 09:28:18 -0600

    Setting a view's frame

--- Sundell_SwiftUI/Sundell_SwiftUI/ContentView.swift
+++ Sundell_SwiftUI/Sundell_SwiftUI/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Image(systemName: "calendar")
+            .resizable()
+            .frame(width: 50, height: 50)
+            .background(Color.red)
     }
 }
 
