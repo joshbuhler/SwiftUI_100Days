741faf9fa8d0196bb3622d8f4b9c74ab7fe86b52
Joshua Buhler <josh@buhler.me>
2020-04-14 09:29:11 -0600

    Adding files for day36

---
+++ Day36_Project7/Day36_Project7/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day36_Project7
+//
+//  Created by Joshua Buhler on 4/14/20.
+//  Copyright © 2020 jb. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
